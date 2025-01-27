86925f6787ca2cf018139a3385276fc935c7ecb0
MacBook Air <macbookair@MacBook-Air-MacBook.local>
2025-01-27 11:50:09 +0500

    add

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Box {...css.navbar}>
+            <Box className='container'>
+                
+            </Box>
+        </Box>
+    );
+}
+
+export default Navbar;
+
+const css = {
+    navbar: {
+        padding: "12px 0"
+    }
+}
